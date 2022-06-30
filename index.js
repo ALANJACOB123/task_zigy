96d6c79463db2e0e65b57aec730a458bf6853b9c
Alan Jacob <alanjacob433@gmail.com>
2022-06-30 20:00:52 +0530

    The functionality to add and delete a user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const MONGODB_URI =
   "mongodb+srv://alanjacob:Alanandg123@cluster0.7opvu.mongodb.net/User";
@@ -15,7 +16,7 @@ app.use(bodyParser.json());
 app.use(userRoutes);
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(process.env.MONGODB_URI)
   .then(result => {
     console.log("Connnected");
     app.listen(3000);
